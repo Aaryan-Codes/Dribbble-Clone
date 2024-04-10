02057a5c84c08d495a8ae4ce12446021a231e488
Aaryan Singh <155874314+Aaryan-Codes@users.noreply.github.com>
2024-04-10 13:12:17 +0530

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -6,14 +6,15 @@ const cors = require('cors');
 const PORT = process.env.PORT || 8000;
 const connectDB = require('./Utils/DB');
 
-var corsOptions = {
-    origin:'https://66163ea2ecc11ea25d59b62f--astounding-bienenstitch-4cccc5.netlify.app/',
-    methods:"GET, POST",
-    credentials:true,
-}
+// var corsOptions = {
+//     origin:'https://66163ea2ecc11ea25d59b62f--astounding-bienenstitch-4cccc5.netlify.app/',
+//     methods:"GET, POST",
+//     credentials:true,
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/info',router);
 
